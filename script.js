b01c097b3b4e27d6c9485b6cb05f3882ec720ce4
Ángel Cruz <langelcruzlara@gmail.com>
2021-08-09 15:51:40 -0500

    feat: implementing workout, running and cycling classes

--- script.js
+++ script.js
@@ -11,6 +11,63 @@ const $inputDuration = document.querySelector('.form__input--duration');
 const $inputCadence = document.querySelector('.form__input--cadence');
 const $inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+
+  /**
+   *
+   * @param {Array} coords [lat, lng]
+   * @param {Number} distance in km
+   * @param {Number} duration in min
+   */
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance;
+    this.duration = duration;
+  }
+}
+
+class Running extends Workout {
+  /**
+   *
+   * @param {Array} coords [lat, lng]
+   * @param {Number} distance in km
+   * @param {Number} duration in min
+   * @param {Number} cadence in step/min
+   */
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  /**
+   *
+   * @param {Array} coords [lat, lng]
+   * @param {Number} distance in km
+   * @param {Number} duration in min
+   * @param {Number} elevationGain in meters
+   */
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
 class App {
   #map;
   #mapEvent;
@@ -30,7 +87,6 @@ class App {
   }
 
   #loadMap(position) {
-    console.log(position);
     const { latitude } = position.coords;
     const { longitude } = position.coords;
 
